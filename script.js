02191e902426a90d03db4c0889e1b24251012292
denisahys <154239616+denisahys@users.noreply.github.com>
2025-02-27 16:09:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 
-    // Seleziona il bottone dell'icona del menu (hamburger) e la lista dei link del menu
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
-
-    // Aggiungi l'evento al clic sull'icona
-    menuToggle.addEventListener('click', () => {
-        // Aggiungi o rimuovi la classe 'active' per aprire/chiudere il menu
-        navLinks.classList.toggle('active');
-    });
