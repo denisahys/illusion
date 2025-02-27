f02cd4e8e45f39c11edc9dab70658c4cfceb01fe
denisahys <154239616+denisahys@users.noreply.github.com>
2025-02-27 12:23:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const burgerMenu = document.querySelector(".burger-menu");
+    const navMenu = document.querySelector(".nav-menu");
+
+    burgerMenu.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+    });
+});
 
