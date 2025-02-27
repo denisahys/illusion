b9fd0b94463a74a473e21f2eae9b46448dd750c9
denisahys <154239616+denisahys@users.noreply.github.com>
2025-02-27 15:19:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-<script>
+
     // Seleziona il bottone dell'icona del menu (hamburger) e la lista dei link del menu
     const menuToggle = document.querySelector('.menu-toggle');
     const navLinks = document.querySelector('.nav-links');
@@ -8,4 +8,3 @@
         // Aggiungi o rimuovi la classe 'active' per aprire/chiudere il menu
         navLinks.classList.toggle('active');
     });
-</script>
